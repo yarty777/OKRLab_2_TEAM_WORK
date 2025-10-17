3c1edd4fd2b4e108dde06501f90c6e2f594d6e76
yarty777 <yaroslav0908l@gmail.com>
2025-10-17 13:34:29 +0300

    Changed a bit placement of objects

--- OKRLR_2/OKRLR_2/Form1.Designer.cs
+++ OKRLR_2/OKRLR_2/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
+            Category = new DataGridViewTextBoxColumn();
+            Sum = new DataGridViewTextBoxColumn();
+            Date = new DataGridViewTextBoxColumn();
+            Comment = new DataGridViewTextBoxColumn();
             comboBox1 = new ComboBox();
             comboBox2 = new ComboBox();
             textBox1 = new TextBox();
@@ -41,10 +45,6 @@
             deleteToolStripMenuItem = new ToolStripMenuItem();
             увійтиToolStripMenuItem = new ToolStripMenuItem();
             заєструватисяToolStripMenuItem = new ToolStripMenuItem();
-            Category = new DataGridViewTextBoxColumn();
-            Sum = new DataGridViewTextBoxColumn();
-            Date = new DataGridViewTextBoxColumn();
-            Comment = new DataGridViewTextBoxColumn();
             label4 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             menuStrip1.SuspendLayout();
@@ -55,109 +55,13 @@
             dataGridView1.BackgroundColor = SystemColors.Control;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Category, Sum, Date, Comment });
-            dataGridView1.Location = new Point(233, 136);
+            dataGridView1.Location = new Point(150, 65);
+            dataGridView1.Margin = new Padding(2);
             dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersVisible = false;
             dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(564, 342);
+            dataGridView1.Size = new Size(554, 307);
             dataGridView1.TabIndex = 0;
-            // 
-            // comboBox1
-            // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(16, 136);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(159, 33);
-            comboBox1.TabIndex = 1;
-            // 
-            // comboBox2
-            // 
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(16, 210);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(159, 33);
-            comboBox2.TabIndex = 2;
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(16, 282);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(159, 31);
-            textBox1.TabIndex = 3;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(16, 108);
-            label1.Name = "label1";
-            label1.Size = new Size(69, 25);
-            label1.TabIndex = 4;
-            label1.Text = "Місяць";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(16, 182);
-            label2.Name = "label2";
-            label2.Size = new Size(89, 25);
-            label2.TabIndex = 5;
-            label2.Text = "Категорія";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(16, 254);
-            label3.Name = "label3";
-            label3.Size = new Size(68, 25);
-            label3.TabIndex = 6;
-            label3.Text = "Пошук";
-            // 
-            // menuStrip1
-            // 
-            menuStrip1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            menuStrip1.Dock = DockStyle.None;
-            menuStrip1.ImageScalingSize = new Size(24, 24);
-            menuStrip1.Items.AddRange(new ToolStripItem[] { addToolStripMenuItem, editToolStripMenuItem, deleteToolStripMenuItem, увійтиToolStripMenuItem, заєструватисяToolStripMenuItem });
-            menuStrip1.Location = new Point(395, 19);
-            menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(485, 36);
-            menuStrip1.TabIndex = 7;
-            menuStrip1.Text = "menuStrip1";
-            menuStrip1.ItemClicked += menuStrip1_ItemClicked;
-            // 
-            // addToolStripMenuItem
-            // 
-            addToolStripMenuItem.Font = new Font("Segoe UI", 10F);
-            addToolStripMenuItem.Name = "addToolStripMenuItem";
-            addToolStripMenuItem.Size = new Size(65, 32);
-            addToolStripMenuItem.Text = "Add";
-            // 
-            // editToolStripMenuItem
-            // 
-            editToolStripMenuItem.Font = new Font("Segoe UI", 10F);
-            editToolStripMenuItem.Name = "editToolStripMenuItem";
-            editToolStripMenuItem.Size = new Size(62, 32);
-            editToolStripMenuItem.Text = "Edit";
-            // 
-            // deleteToolStripMenuItem
-            // 
-            deleteToolStripMenuItem.Font = new Font("Segoe UI", 10F);
-            deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
-            deleteToolStripMenuItem.Size = new Size(84, 32);
-            deleteToolStripMenuItem.Text = "Delete";
-            // 
-            // увійтиToolStripMenuItem
-            // 
-            увійтиToolStripMenuItem.Font = new Font("Segoe UI", 10F);
-            увійтиToolStripMenuItem.Name = "увійтиToolStripMenuItem";
-            увійтиToolStripMenuItem.Size = new Size(87, 32);
-            увійтиToolStripMenuItem.Text = "Увійти";
-            // 
-            // заєструватисяToolStripMenuItem
-            // 
-            заєструватисяToolStripMenuItem.Font = new Font("Segoe UI", 10F);
-            заєструватисяToolStripMenuItem.Name = "заєструватисяToolStripMenuItem";
-            заєструватисяToolStripMenuItem.Size = new Size(179, 32);
-            заєструватисяToolStripMenuItem.Text = "Зареєструватись";
             // 
             // Category
             // 
@@ -171,12 +75,14 @@
             Sum.HeaderText = "Сума";
             Sum.MinimumWidth = 8;
             Sum.Name = "Sum";
+            Sum.Width = 125;
             // 
             // Date
             // 
             Date.HeaderText = "Дата";
             Date.MinimumWidth = 8;
             Date.Name = "Date";
+            Date.Width = 125;
             // 
             // Comment
             // 
@@ -185,23 +91,129 @@
             Comment.Name = "Comment";
             Comment.Width = 150;
             // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(13, 109);
+            comboBox1.Margin = new Padding(2);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(128, 28);
+            comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(13, 168);
+            comboBox2.Margin = new Padding(2);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(128, 28);
+            comboBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(13, 226);
+            textBox1.Margin = new Padding(2);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(128, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(13, 86);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(58, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Місяць";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(13, 146);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Категорія";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(13, 203);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(55, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Пошук";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            menuStrip1.Dock = DockStyle.None;
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { addToolStripMenuItem, editToolStripMenuItem, deleteToolStripMenuItem, увійтиToolStripMenuItem, заєструватисяToolStripMenuItem });
+            menuStrip1.Location = new Point(289, 22);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Padding = new Padding(5, 2, 0, 2);
+            menuStrip1.Size = new Size(416, 31);
+            menuStrip1.TabIndex = 7;
+            menuStrip1.Text = "menuStrip1";
+            menuStrip1.ItemClicked += menuStrip1_ItemClicked;
+            // 
+            // addToolStripMenuItem
+            // 
+            addToolStripMenuItem.Font = new Font("Segoe UI", 10F);
+            addToolStripMenuItem.Name = "addToolStripMenuItem";
+            addToolStripMenuItem.Size = new Size(55, 27);
+            addToolStripMenuItem.Text = "Add";
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.Font = new Font("Segoe UI", 10F);
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(53, 27);
+            editToolStripMenuItem.Text = "Edit";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            deleteToolStripMenuItem.Font = new Font("Segoe UI", 10F);
+            deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            deleteToolStripMenuItem.Size = new Size(73, 27);
+            deleteToolStripMenuItem.Text = "Delete";
+            // 
+            // увійтиToolStripMenuItem
+            // 
+            увійтиToolStripMenuItem.Font = new Font("Segoe UI", 10F);
+            увійтиToolStripMenuItem.Name = "увійтиToolStripMenuItem";
+            увійтиToolStripMenuItem.Size = new Size(74, 27);
+            увійтиToolStripMenuItem.Text = "Увійти";
+            // 
+            // заєструватисяToolStripMenuItem
+            // 
+            заєструватисяToolStripMenuItem.Font = new Font("Segoe UI", 10F);
+            заєструватисяToolStripMenuItem.Name = "заєструватисяToolStripMenuItem";
+            заєструватисяToolStripMenuItem.Size = new Size(154, 27);
+            заєструватисяToolStripMenuItem.Text = "Зареєструватись";
+            // 
             // label4
             // 
             label4.AutoSize = true;
             label4.BackColor = Color.Transparent;
             label4.Font = new Font("Segoe UI Black", 20F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            label4.Location = new Point(12, 9);
+            label4.Location = new Point(10, 7);
+            label4.Margin = new Padding(2, 0, 2, 0);
             label4.Name = "label4";
-            label4.Size = new Size(331, 54);
+            label4.Size = new Size(284, 46);
             label4.TabIndex = 8;
             label4.Text = "ExpenseTracker";
             label4.Click += label4_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1079, 499);
+            ClientSize = new Size(709, 383);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label2);
@@ -212,6 +224,7 @@
             Controls.Add(dataGridView1);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
+            Margin = new Padding(2);
             Name = "Form1";
             Text = "ExpenseTracker";
             Load += Form1_Load;
